fb7780bbc45ef073d8544bb88c20a486ec2f98f9
Petar Petrov <ppetrov@vexelon.net>
2024-04-23 21:15:35 +0200

    Add more unit tests

--- translitbg_test.go
+++ translitbg_test.go
@@ -21,8 +21,10 @@ func TestSentences(t *testing.T) {
 		{"Ѝ може да бъде намерен и в други езици \nкато руския език и украинския език.", "I mozhe da bade nameren i v drugi ezitsi \nkato ruskia ezik i ukrainskia ezik."},
 	}
 
+	tr := New()
+
 	for _, tc := range testCases {
-		output, _ := New().Run(tc.input)
+		output, _ := tr.Run(tc.input)
 
 		if output != tc.expected {
 			t.Errorf("For sentence '%s', expected '%s', got '%s'", tc.input, tc.expected, output)
@@ -41,8 +43,10 @@ func TestPeopleNames(t *testing.T) {
 		{"Явор", "Yavor"},
 	}
 
+	tr := New()
+
 	for _, tc := range testCases {
-		output, _ := New().Run(tc.input)
+		output, _ := tr.Run(tc.input)
 
 		if output != tc.expected {
 			t.Errorf("For name '%s', expected '%s', got '%s'", tc.input, tc.expected, output)
@@ -69,8 +73,179 @@ func TestOtherNames(t *testing.T) {
 		{"Горна Оряховица", "Gorna Oryahovitsa"},
 	}
 
+	tr := New()
+
 	for _, tc := range testCases {
-		output, _ := New().Run(tc.input)
+		output, _ := tr.Run(tc.input)
+
+		if output != tc.expected {
+			t.Errorf("For other name '%s', expected '%s', got '%s'", tc.input, tc.expected, output)
+		}
+	}
+}
+
+func Test100Names(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected string
+	}{
+		{"Здравей", "Zdravey"},
+		{"Благодаря", "Blagodarya"},
+		{"Моля", "Molya"},
+		{"Добре", "Dobre"},
+		{"Ден", "Den"},
+		{"Нощ", "Nosht"},
+		{"Човек", "Chovek"},
+		{"Град", "Grad"},
+		{"Страна", "Strana"},
+		{"Живот", "Zhivot"},
+		{"Любов", "Lyubov"},
+		{"Семейство", "Semeystvo"},
+		{"Вода", "Voda"},
+		{"Храна", "Hrana"},
+		{"Къща", "Kashta"},
+		{"Работа", "Rabota"},
+		{"Училище", "Uchilishte"},
+		{"Университет", "Universitet"},
+		{"Книга", "Kniga"},
+		{"Музика", "Muzika"},
+		{"Изкуство", "Izkustvo"},
+		{"История", "Istoria"},
+		{"Време", "Vreme"},
+		{"Новина", "Novina"},
+		{"Път", "Pat"},
+		{"Автомобил", "Avtomobil"},
+		{"Телефон", "Telefon"},
+		{"Ресторант", "Restorant"},
+		{"Летище", "Letishte"},
+		{"Парк", "Park"},
+		{"Животно", "Zhivotno"},
+		{"Риба", "Riba"},
+		{"Птица", "Ptitsa"},
+		{"Цвете", "Tsvete"},
+		{"Дърво", "Darvo"},
+		{"Планина", "Planina"},
+		{"Река", "Reka"},
+		{"Слънце", "Slantse"},
+		{"Луна", "Luna"},
+		{"Звезда", "Zvezda"},
+		{"Цвят", "Tsvyat"},
+		{"Денс", "Dens"},
+		{"Пеене", "Peene"},
+		{"Гледам", "Gledam"},
+		{"Слушам", "Slusham"},
+		{"Говоря", "Govorya"},
+		{"Спорт", "Sport"},
+		{"Игра", "Igra"},
+		{"Смях", "Smyah"},
+		{"Природа", "Priroda"},
+		{"Обичам", "Obicham"},
+		{"Сърце", "Sartse"},
+		{"Тяло", "Tyalo"},
+		{"Ръка", "Raka"},
+		{"Крак", "Krak"},
+		{"Очи", "Ochi"},
+		{"Уста", "Usta"},
+		{"Коса", "Kosa"},
+		{"Нос", "Nos"},
+		{"Ухо", "Uho"},
+		{"Език", "Ezik"},
+		{"Зъб", "Zab"},
+		{"Глава", "Glava"},
+		{"Кожа", "Kozha"},
+		{"Нокът", "Nokat"},
+		{"Кръв", "Krav"},
+		{"Въздух", "Vazduh"},
+		{"Огън", "Ogan"},
+		{"Земя", "Zemya"},
+		{"Вода", "Voda"},
+		{"Мъгла", "Magla"},
+		{"Вятър", "Vyatar"},
+		{"Сняг", "Snyag"},
+		{"Дъжд", "Dazhd"},
+		{"Топло", "Toplo"},
+		{"Студено", "Studeno"},
+		{"Храна", "Hrana"},
+		{"Питие", "Pitie"},
+		{"Спане", "Spane"},
+		{"Бързо", "Barzo"},
+		{"Бавно", "Bavno"},
+		{"Силно", "Silno"},
+		{"Слабо", "Slabo"},
+		{"Голям", "Golyam"},
+		{"Малък", "Malak"},
+		{"Дълъг", "Dalag"},
+		{"Кратък", "Kratak"},
+		{"Тежък", "Tezhak"},
+		{"Лек", "Lek"},
+		{"Стар", "Star"},
+		{"Млад", "Mlad"},
+		{"Нов", "Nov"},
+		{"Стар", "Star"},
+		{"Добър", "Dobar"},
+		{"Лош", "Losh"},
+		{"Горещ", "Goresht"},
+		{"Студен", "Studen"},
+		{"Богат", "Bogat"},
+		{"Беден", "Beden"},
+		{"Висок", "Visok"},
+		{"Нисък", "Nisak"},
+		{"Дълбок", "Dalbok"},
+		{"Плитък", "Plitak"},
+	}
+
+	tr := New()
+
+	for _, tc := range testCases {
+		output, _ := tr.Run(tc.input)
+
+		if output != tc.expected {
+			t.Errorf("For other name '%s', expected '%s', got '%s'", tc.input, tc.expected, output)
+		}
+	}
+}
+
+func TestCityNames(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected string
+	}{
+		{"София", "Sofia"},
+		{"Пловдив", "Plovdiv"},
+		{"Варна", "Varna"},
+		{"Бургас", "Burgas"},
+		{"Русе", "Ruse"},
+		{"Стара Загора", "Stara Zagora"},
+		{"Плевен", "Pleven"},
+		{"Сливен", "Sliven"},
+		{"Добрич", "Dobrich"},
+		{"Шумен", "Shumen"},
+		{"Хасково", "Haskovo"},
+		{"Перник", "Pernik"},
+		{"Ямбол", "Yambol"},
+		{"Благоевград", "Blagoevgrad"},
+		{"Велико Търново", "Veliko Tarnovo"},
+		{"Враца", "Vratsa"},
+		{"Габрово", "Gabrovo"},
+		{"Видин", "Vidin"},
+		{"Монтана", "Montana"},
+		{"Ловеч", "Lovech"},
+		{"Разград", "Razgrad"},
+		{"Силистра", "Silistra"},
+		{"Търговище", "Targovishte"},
+		{"Кюстендил", "Kyustendil"},
+		{"Пазарджик", "Pazardzhik"},
+		{"Смолян", "Smolyan"},
+		{"Кърджали", "Kardzhali"},
+		{"Велинград", "Velingrad"},
+		{"Дупница", "Dupnitsa"},
+		{"Петрич", "Petrich"},
+	}
+
+	tr := New()
+
+	for _, tc := range testCases {
+		output, _ := tr.Run(tc.input)
 
 		if output != tc.expected {
 			t.Errorf("For other name '%s', expected '%s', got '%s'", tc.input, tc.expected, output)
