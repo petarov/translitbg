4b3333f2e34550af006a845045dfe0871d2b46a3
Petar Petrov <ppetrov@vexelon.net>
2024-04-24 08:18:44 +0200

    Bump documentation

--- doc.go
+++ doc.go
@@ -1,6 +1,10 @@
-// Package translitbg is a Bulgarian-language transliteration package. The API
-// can be used in a very simple way to encode cyrillic-characters text to its
-// corresponding latin-characters version.
+// Package translitbg is a Bulgarian-language transliteration package.
+// It complies with the Bulgarian transliteration law which describes the
+// "Streamlined System for the Romanization of Bulgarian" as the official
+// transliteration method in the country.
+//
+// The API can be used in a very simple way to encode cyrillic-characters text
+// to its corresponding latin-characters version.
 //
 // Example:
 //
@@ -11,4 +15,11 @@
 // There are no state-related parameters stored in the translitbg object, so
 // the same instance can be used to encode more text without the need to create
 // new objects every time.
+//
+//	tr := translitbg.New()
+//	tr.Encode("Стара планина")
+//	// Output: Stara planina
+//
+//	tr.Encode("Горна Оряховица")
+//	// Output: Gorna Oryahovitsa
 package translitbg
