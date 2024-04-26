8a4ce787c8644846a4caae4089b2a88d266288d4
Petar Petrov <ppetrov@vexelon.net>
2024-04-26 09:39:28 +0200

    Fix documentation indentation and add comments

--- doc.go
+++ doc.go
@@ -16,17 +16,17 @@
 // the same instance can be used to encode more text without the need to create
 // new objects every time.
 //
-//		tr := translitbg.New()
-//		tr.Encode("Стара планина")
-//		// Output: Stara planina
+//	tr := translitbg.New()
+//	tr.Encode("Стара планина")
+//	// Output: Stara planina
 //
-//		tr.Encode("Горна Оряховица")
-//		// Output: Gorna Oryahovitsa
+//	tr.Encode("Горна Оряховица")
+//	// Output: Gorna Oryahovitsa
 //
-//	 There is an edge case where according to Bulgarian law the name "БЪЛГАРИЯ"
-//	 is an exception to the transliteration rule and must have its "Ъ"
-//	 transformed into a "U" instead of an "A". For example:
+// There is an edge case where according to Bulgarian law the name "БЪЛГАРИЯ"
+// is an exception to the transliteration rule and must have its "Ъ"
+// transformed into a "U" instead of an "A". For example:
 //
-//		tr.Encode("БЪЛГАРИЯ")
-//		// Output: BULGARIA
+//	tr.Encode("БЪЛГАРИЯ")
+//	// Output: BULGARIA
 package translitbg
